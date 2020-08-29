7b131e2bad6ab9982d7f13d33bb295d63d5ea085
Dean <iamdekanos@gmail.com>
2020-08-29 09:42:51 +0300

    fixes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,25 +1,26 @@
-const weatherForm = document.querySelector('form');
-const search = document.querySelector('input');
-const messageOne = document.getElementById('message-1');
-const messageTwo = document.getElementById('message-2')
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.getElementById("message-1");
+const messageTwo = document.getElementById("message-2");
 
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+  const location = search.value;
 
-    const location = search.value;
+  messageOne.textContent = "Calculating...";
+  messageTwo.textContent = "";
 
-    messageOne.textContent = 'Calculating...';
-    messageTwo.textContent = "";
-
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-             messageOne.textContent = data.error; 
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
         } else {
-        messageOne.textContent = data.location;
-        messageTwo.textContent = data.forecast;
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
         }
-    })
-})
-})
+      });
+    }
+  );
+});
--- src/app.js
+++ src/app.js
@@ -1,102 +1,104 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const request = require('postman-request');
-const geocode = require('./utils/geocode');
-const forecast = require('./utils/forecast')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const request = require("postman-request");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const app = express()
+const app = express();
 
 // DEFINE PATHS FOR EXPRESS CONFIG
-const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialPath = path.join(__dirname, '../templates/partials')
-
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
 
 // SETUP HANDLEBARS ENGINE AND VIEWS LOCATION
-app.set('view engine', 'hbs');
-app.set('views', viewsPath)
-hbs.registerPartials(partialPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialPath);
 
 // SETUP STATIC DIRECTORY TO SERVE
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: "WEATHER",
-        name: 'Dean',
-    })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "WEATHER",
+    name: "Dean",
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: "ABOUT PAGE",
-        name: 'Deanos'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "ABOUT PAGE",
+    name: "Deanos",
+  });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: "HELP PAGE",
-        name: 'Dean'
-    })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "HELP PAGE",
+    name: "Dean",
+  });
+});
 
-app.get('/weather', (req, res) => {
-    if(!req.query.address){
-        return res.send({
-            error: 'You much provide a proper address'
-        })
-    }
-    geocode(req.query.address, (error, { latitude, longtitude, location} ={}) => {
-        if(error) {
-            return res.send({error})
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You much provide a proper address",
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longtitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longtitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
         }
 
-        forecast(latitude, longtitude, (error, forecastData) => {
-            if(error) {
-                return res.send({error})
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
-    })
-})
-
-app.get('/products', (req, res) => {
-    if(!req.query.search){
-        return res.send({
-            error: 'You much provide a proper search'
-        })
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
     }
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
+  );
+});
 
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'DEANOS',
-        error: 'THERE IS NO HELP HERE'
-    })
-})
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You much provide a proper search",
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
 
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'DEANOS',
-        error: 'This is an error page message'
-    })
-})
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "DEANOS",
+    error: "THERE IS NO HELP HERE",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "DEANOS",
+    error: "This is an error page message",
+  });
+});
 
 const PORT = process.env.port || 3000;
 
-app.listen(PORT , () => {
-    console.log('app is running on port ' + PORT)
-})
+app.listen(PORT, () => {
+  console.log("app is running on port " + PORT);
+});
